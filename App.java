1fbb1258b0c7eb19acd756805e083d623c1dca28
Ashish Jonchhen <jonchhen.ashish@hotmail.com>
2022-06-09 16:19:11 -0500

    changes

--- App.java
+++ App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * main
@@ -5,6 +7,25 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        int[] array = { 1, 5, 2, 8, 3, 4 };
+        int inversionCount = 0;
+
+        List<Integer> pairs = new ArrayList<>();
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    inversionCount++;
+                    pairs.add(array[i]);
+                    pairs.add(array[j]);
+                }
+
+            }
+
+        }
+        System.out.println("Inversion count: " + inversionCount);
+        System.out.println(pairs);
+
     }
 }
